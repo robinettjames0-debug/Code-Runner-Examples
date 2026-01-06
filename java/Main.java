f1e00395586db48f57cafaab4fbf6e22415e0969
robinettjames0-debug <232507229+robinettjames0-debug@users.noreply.github.com>
2026-01-06 13:16:29 -0600

    Update Main.java

--- java/Main.java
+++ java/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main {/usr/local/openjdk22/bin/javac Main.java Utils.java
     public static void main(String[] args) {
         String greeting = Module.greeting();
         System.out.println(greeting);
