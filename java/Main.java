eaa2928f10f96d499e686c4a95ad641775d5bb27
robinettjames0-debug <232507229+robinettjames0-debug@users.noreply.github.com>
2026-01-06 13:18:53 -0600

    Update Main.java

--- java/Main.java
+++ java/Main.java
@@ -1,6 +1,11 @@
 public class Main {/usr/local/openjdk22/bin/javac Main.java Utils.java
     public static void main(String[] args) {/usr/local/openjdk22/bin/java Main
-        String greeting = Module.greeting();
+        String greeting = Module.greeting(project/
+├── mypackage/
+│   ├── __init__.py
+│   └── helper.py
+├── main.py
+└── run);
         System.out.println(greeting);
     }
 }
